34285573082acd173aa2c755c569bd3fa65f65c6
yc-song <57248081+yc-song@users.noreply.github.com>
2020-06-15 14:06:52 +0900

    Update gpagame.py

--- gpagame.py
+++ gpagame.py
@@ -15,11 +15,10 @@ import pandas as pd
 df = pd.read_csv('db.csv', encoding='utf-8') #csv 불러옴
 dev=[] #편차 저장을 위한 list
 present=[]
-# estimated[df["이름"]]=float(df["예상학점"])
-# real[df["이름"]]=float(df["자기학점"])
 
 a=0
 r=0
+u=0
 M=len(df['name'])
 for value in df['own']:
     if float(value)>0:
@@ -33,7 +32,6 @@ for value in df['est']:
     dev.append(float(abs(average-value))) #예상학점 대비 편차 계산
 
 df["dev"]=dev
-u=0
 for i,j in zip(df['name'],df['dev']): #편차 1 넘으면 실격
     if j>=1:
         df=df.drop(u)
